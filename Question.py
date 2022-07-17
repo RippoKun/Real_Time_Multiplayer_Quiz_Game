163371f2c1e0ef8033b58e3ab3f4c6063ce41bee
RippoKun <106089122+RippoKun@users.noreply.github.com>
2022-07-17 13:50:03 +0800

    Create Question.py

---
+++ Question.py
@@ -0,0 +1,18 @@
+Q_and_A = [['The IETF standards documents are called ________:', ["RFC", "RCF", "ID", "DFC"], "RFC"],
+		  ['What country has the highest life expectancy?',["Hong Kong", "China", "USA", "Malaysia"], "Hong Kong"],
+		  ['Where would you be if you were standing on the Spanish Steps?',["London", "New York", "Rome", "Prague"], "Rome"],
+		  ['What is the most common surname in the United States?',["Glazer", "Smith", "Sam", "Gerrard"], "Smith"],
+		  ['Who was the Ancient Greek God of the Sun?' ,["Zeus", "Poseidon", "Hela", "Apollo"], "Apollo"],
+		  ['Who won the 2010 FIFA World Cup?' ,["Argentina", "Spain", "Portugal", "Brazil"], "Spain"],
+		  ['How many minutes are in a full week?',["10080", "11009", "12000", "13230"], "10080"],
+		  ['When is the Independence Day of Malaysia?', ["31 August", "16 September", "23 February", "15 January"], "31 August"],
+		  ['Which country was the host of the UEFA Euro 2016?', ["Russia", "Poland and Ukraine", "France", "England"], "France"],
+		  ['What character has both Robert Downey Jr. and Benedict Cumberbatch played?' ,["Willy Wonka", "Jason Bourne", "Sherlock Holmes", "Tony Stark"], "Sherlock Holmes"],
+		  ['What was Meta Platforms Inc formerly known as?' ,["Instagram", "Facebook", "MySpace", "Twitter"], "Facebook"],
+		  ['Which element is said to keep bones strong?' ,["Potassium", "Iron", "Calcium", "Vibranium"], "Calcium"],
+		  ['Who is the leading star of James Bond: Spectre?', ["Daniel Craig", "Cillian Murphy", "Tom Cruise", "Dwayne Johsonn"], "Daniel Craig"],
+		  ['Who was the singer of the song "Cant Help Falling In Love"?', ["Elvis Presley", "The Beatles", "Michael Jackson", "James Arthur"], "Elvis Presley"],
+		  ['What is the main character for the British-series "Peaky Blinders"?', ["Michael Gray", "Thomas Shelby", "Steve Rogers", "Bruce Wayne"], "Thomas Shelby"],
+		  ['Which country has a nickname of "The Kiwis"?', ["Australia", "United Kingdom", "New Zealand", "Brazil"], "New Zealand"]]
+		  
+		
